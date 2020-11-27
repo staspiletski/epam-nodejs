1a45d532b3ae34ab673b28da348828b649d8d23d
Stanislau Piletski <Stanislau.Piletski@gaf.com>
2020-11-27 11:07:11 +0300

    hw 7 - test updates

--- src/tests/userController.test.ts
+++ src/tests/userController.test.ts
@@ -1,10 +1,34 @@
 import express from 'express';
 import request from 'supertest';
 import { userRouter } from '../routes/userRoutes';
+import userService from '../services/userServices';
+import Mock = jest.Mock;
 
+jest.mock('../services/userServices');
+/*, () => ({
+  userService: {
+    readAll: jest.fn(),
+    updateUser: jest.fn(),
+    getUserById: jest.fn(),
+    getAutoSuggestUsers: jest.fn(),
+    markUserDeleted: jest.fn()
+  }
+}));
+*/
 const app = express().use(express.json()).use(userRouter);
 
+const data = {
+  login: 'login name 1',
+  password: 'password 12345',
+  age: 35,
+  isDeleted: false,
+};
+
 describe('UserController', () => {
+  (userService.readAll as Mock).mockResolvedValue([
+    { id: '123', login: 'login1', password: 'TestPassword123', age: 54 }
+  ]);
+
   it('GET /user', done => {
     request(app)
       .get('/user')
@@ -39,7 +63,7 @@ describe('UserController', () => {
       });
   });
 
-  it('GET /user/:id', done => {
+  it('GET /user/:id user not found', done => {
     request(app)
       .get('/user/9cc90bfb-5555-47b9-86a9-4a2be7954f60')
       .set('Accept', 'application/json')
@@ -51,31 +75,18 @@ describe('UserController', () => {
       });
   });
 
-  let data = {
-    login: 'login name 1',
-    password: 'password 12345',
-    age: 35,
-    isDeleted: false,
-  };
-
   it('POST /user', done => {
+    // @ts-ignore
+    userService.create.mockResolvedValue(data);
     request(app)
       .post('/user')
+      .send(data)
       .set('Accept', 'application/json')
-      /* .send(data)
       .expect('Content-Type', /json/)
-      .expect(201)*/
-      .end((err, res) => {
-        if (err) {
-          console.log('error --- ', err);
-          done(err);
-        } else {
-          console.log(' res --- ', res);
-          expect(res.body).toEqual({
-            ...data,
-          });
+      .expect(201)
+      .end((err) => {
+        if (err) return done(err);
           done();
-        }
       });
 
     /*   .then(response => {
